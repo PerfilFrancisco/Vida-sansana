ee992169827df7f539298940137f951f4ab3cb97
zazon222 <franciscosanchez11182@gmail.com>
2022-11-15 00:18:27 -0300

    actualizacion mediana
    cambie algunas cosas mas, continuare en la mañana y preguntare al brian si me puede ayudar con unas cosas

--- proyecto/vidaSansana/views.py
+++ proyecto/vidaSansana/views.py
@@ -5,3 +5,5 @@ def crearTaller(request):
     return render(request,'crear_taller.html')
 def inicio(request):
     return render(request,'inicio.html')
+def crearPublicacion(request):
+    return render(request,'crear_publicacion.html')
