18d1a3c8c0c5aa0b7f300670387e88b3d3e22df3
nottfenrir <fabianbustosby@gmail.com>
2022-11-15 16:43:03 -0300

    nada

--- proyecto/vidaSansana/models.py
+++ proyecto/vidaSansana/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class sede(models.Model):
+    sede_id = models.AutoField(primary_key=True)
+    sede_nombre = models.CharField(max_length=45)
+
+
 class taller(models.Model):
     tall_id = models.AutoField(primary_key=True)
     tall_nombre = models.CharField(max_length=45)
@@ -9,4 +14,7 @@ class taller(models.Model):
     tall_descripcion = models.CharField(max_length=45)
     tall_cupo_act = models.IntegerField
     tall_cupo_max = models.IntegerField
-    
+    tall_estado = models.BinaryField
+    tall_ubicacion = models.CharField(max_length=45)
+    sede_id = models.ForeignKey(sede,on_delete = models.CASCADE)
+
